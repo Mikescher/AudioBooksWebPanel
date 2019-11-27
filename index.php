df849c87528a23cb22baad64e7a350bb3b25271e
Mike Schwörer <mailport@mikescher.de>
2019-11-27 19:11:33 +0100

    inital state (mostly copied from ClipCornWebPanel)

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>AudioBooks - Webview</title>
+	<link rel="icon" type="image/png" href="/data/icon.png"/>
+    <link rel="stylesheet" href="/data/styles.css"/>
+</head>
+<body>
+
+	<div id="root">
+
+	</div>
+
+    <script src="/data/jquery-3.4.1.js" type="text/javascript" ></script>
+    <script src="/data/script.js" type="text/javascript" ></script>
+
+</body>
+</html>
